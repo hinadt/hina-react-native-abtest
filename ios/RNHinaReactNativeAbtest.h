90508ea1da4955501d534602df8295c778da016e
邓昊 <denghao@hinadt.com>
2024-09-14 14:37:36 +0800

    add iOS bridge file

--- ios/RNHinaReactNativeAbtest.h
+++ ios/RNHinaReactNativeAbtest.h
@@ -1,9 +1,14 @@
+#import <Foundation/Foundation.h>
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
+#import <Foundation/Foundation.h>
+
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#else
+#import "RCTBridgeModule.h"
 #endif
+
+
 
 @interface RNHinaReactNativeAbtest : NSObject <RCTBridgeModule>
 
